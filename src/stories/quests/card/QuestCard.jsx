82335de623e70997bcab6aa4ad23b0ae4a0669f8
Kathy Tavia <42288986+cdrmr18@users.noreply.github.com>
2024-06-10 10:46:10 +0200

    add check and close icons & style

--- src/stories/quests/card/QuestCard.jsx
+++ src/stories/quests/card/QuestCard.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { BsDownload } from "react-icons/bs";
 import { BsArrowUpRight } from "react-icons/bs";
+import { IoCloseOutline } from "react-icons/io5";
+import { IoIosCheckmark } from "react-icons/io";
 import "./questCard.scss";
 
 export const QuestCard = ({ title, type }) => {
@@ -12,7 +14,9 @@ export const QuestCard = ({ title, type }) => {
         <BsDownload className={`quest-card__logo quest-card__logo--${type}`} />
       </div>
       <div className="quest-card__content-wrapper">
-        <h3 className="quest-card__title">{title}</h3>
+        <h3 className="quest-card__title">
+          Install the CareerOS Chrome Extension
+        </h3>
         <p className="quest-card__content">
           Unlock the superpowers of CareerOS with our Chrome Extension. This
           plugin lets you save companies, contacts, and jobs from Linkedin and
@@ -20,8 +24,14 @@ export const QuestCard = ({ title, type }) => {
           send...
         </p>
       </div>
-      <div className="quest-card__action-wrapper">
-        <BsArrowUpRight className="quest-card__action-icon" />
+      <div>
+        <div className="quest-card__close">
+          <IoIosCheckmark />
+          <IoCloseOutline />
+        </div>
+        <div className="quest-card__action-wrapper">
+          <BsArrowUpRight className="quest-card__action-icon" />
+        </div>
       </div>
     </div>
   );
